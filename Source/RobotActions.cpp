f6446cb12db08a3cfa8ae3df0de6359dae16ac8a
Dallas Fletchall <dallas.fletchall@gmail.com>
2014-11-28 12:47:22 -0600

    Fix robotMotions to more appropriate name RobotActions

--- Source/RobotActions.cpp
+++ Source/RobotActions.cpp
@@ -6,7 +6,11 @@
  */
 #include <cmath>
 #include <iostream>
-#include "robotMotions.hpp"
+#include "RobotActions.hpp"
+
+#define DISTANCE_STOP_BUFFER 10
+#define ANGLE_STOP_BUFFER 5
+#define CONSECUTIVE_NON_MOTIONS 5
 
 using namespace std;
 
@@ -47,5 +51,15 @@ void trackRobot( ArRobot *robot, double dist, double dir ){
    robot->unlock();
 
    translate(robot, dist);
+
+}
+
+void isRobotTracking( int *count, double dist, double angle ){
+
+   if( abs(dist) > DISTANCE_STOP_BUFFER || abs(angle) > ANGLE_STOP_BUFFER){
+      *count = *count + 1;
+   }else{
+      *count = CONSECUTIVE_NON_MOTIONS;
+   }
 
 }
